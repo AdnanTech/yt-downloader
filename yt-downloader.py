fa5a79847af3f41d5744fa99f35bb37a13e2eeab
Adnan Tech <adnanquisar0@gmail.com>
2020-08-02 16:54:14 +0100

    Initialised

---
+++ yt-downloader.py
@@ -0,0 +1,21 @@
+# ----------------------------------- OVERVIEW -----------------------------------
+
+#           Goal: Create a program to download a YouTube videos
+#           Objective: Create a progra that downloads a YouTube video from a URL as mp3/mp4 format
+
+# ----------------------------------- SOURCE CODE -----------------------------------
+from datetime import datetime
+
+start_time = datetime.now()
+
+def initialize():
+    print('Starting')
+
+def main():
+    print('Code goes here')
+
+initialize()
+main()
+
+end_time = datetime.now()
+print('Duration: {}'.format(end_time - start_time))
